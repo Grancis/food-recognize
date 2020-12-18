36da84640c2e4f436270eca80d0afdb4c43f1f1b
Grancis <wzhenchao@live.com>
2020-12-18 15:20:30 +0800

    更新：修改main.py文件

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import config
-from src.load_data.image_list import get_image_list
-from src.load_data.dataset_custom import DatasetCustom
 
-from torchvision import transforms
-from torch.utils.data import DataLoader
+from src.model.train import train_model
+from src.model.test import test
+from src.load_data.get_dataloader import get_loader
+
+def do_train():
+    print('train start')
+    train_model()
+    print('train done')
+
+def do_test():
+    test_loader = get_loader(config.DATA_TEST_ROOT)
+    test(test_loader)
 
 if __name__ == '__main__':
-    image_list = get_image_list('/Users/grancis/Downloads/release_data/train_list.txt')
-    transform1 = transforms.Compose([
-        transforms.Resize(300),
-        transforms.CenterCrop(300),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[.4, .4, .4], std=[.2, .2, .2])
-    ])
-    dataset_train = DatasetCustom('/Users/grancis/Downloads/release_data/train', image_list, transform=transform1)
-    print(len(dataset_train))
-    print(dataset_train[0])
-    train_loader = DataLoader(dataset_train, batch_size=1, num_workers=1)
-    # print(train_loader)
+    train_model()
